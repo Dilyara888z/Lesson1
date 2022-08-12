d9282e892b32f924fb3fdc8521ebf12de523d60c
“Dilaura_z” <dilaura_z@mail.ru>
2022-08-12 21:10:12 +0300

    added DZ_task2.1,DZ_task4

--- DZ_Task4/Program.cs
+++ DZ_Task4/Program.cs
@@ -5,13 +5,13 @@
 // 22 3 9 -> 22
 
 Console.Write("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (b > max) max = a;
-if (c > max) max = c;
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 Console.WriteLine($"Максимальное число:{max}");
 
