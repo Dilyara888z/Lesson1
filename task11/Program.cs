58d679f02f548e7fc7a63d28844b37a3b5a1c89d
“Dilaura_z” <dilaura_z@mail.ru>
2022-08-15 18:47:20 +0300

    Выполнено домашнее задание ко второму семинару.

---
+++ task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число
+//  и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100,1000); 
+int num1= number/100;
+int num2 = number%10;
+int result = num1 * 10 + num3;
+Console.WriteLine($"Случайное трехзначное число =>{number}");
+Console.WriteLine($"двузначное число из трехзначного числа => {result}{result2}");
+
+// int number = new Random().Next(100,1000); 
+// int result = number/100;
+// int result2 = number%10;
+// Console.WriteLine($"Случайное трехзначное число =>{number}");
+// Console.WriteLine($"двузначное число из трехзначного числа => {result}{result2}");
---
+++ task9/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая выводит случайное число
+//  из отрезка [10,99] и показывает наибольшую цифру числа
+
+int number = new Random().Next(10,100); // 10, 99+1 тк последнее число не вхоит в  диапозон
+Console.WriteLine($"Случайное число диапазона 10 -99 =>{number}");
+
+int firstDigit = number / 10; // / это деление и выводит целое число
+int secondDigit = number % 10; // % показвает остаток
+
+// if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+// else if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа=>{firstDigit}");
+// else Console.WriteLine($"наибольшаяя цифра числа=>{secondDigit}");
+
+//int result = firstDigit > secondDigit ? firstDigit : secondDigit; // это альтернативный способ написания if и else
+//Console.WriteLine($"наибольшаяя цифра числа=>{result}");
+// что бы добавить еще и сравнение равных чисел:
+
+// string result = firstDigit == secondDigit ? "Цифры равны"
+// : firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+// Console.WriteLine($"наибольшаяя цифра числа=>{result}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num /10;
+    int secondDigit = num % 10;
+    if(firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ?  firstDigit : secondDigit;
+}
+
+int result = MaxDigit(number);
+String res = result == 0 ? "Цифры равны" : result.ToString();
+Console.WriteLine($"наибольшаяя цифра числа=>{res}");
